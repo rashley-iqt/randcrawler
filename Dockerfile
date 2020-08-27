a4353afa0cf7ba7871ed42f1c4181d413f91c290
ryan <rashley@iqt.org>
2020-08-27 11:25:24 -0400

    switch to cron job for monitoring

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ FROM python:3.8-slim-buster
 COPY . .
 
 RUN apt update && \
-    apt install -y tcpdump watch && \
+    apt install -y tcpdump cron net-tools && \
     pip3 install -r requirements.txt
+
+COPY crontab /etc/cron.d/monitor-cron
+RUN chmod 644 /etc/cron.d/monitor-cron
+
 RUN chmod +x entrypoint.sh
-CMD ["/bin/sh", "entrypoint.sh"] 
+CMD ["/bin/bash", "entrypoint.sh"] 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-DIR_NAME="/data/$(date +%F-%H%M)";
-FILE_NAME="$(date +%H-%M-%S)"
-if [ ! -d "$DIR_NAME" ]; then mkdir "$DIR_NAME"; fi
+ARP_DIR="/data/arp"
+RT_DIR="/data/routes"
+CAP_DIR="/data/pcaps"
 
-if [ ! -d "$DIR_NAME/arp" ]; then mkdir "$DIR_NAME/arp"; fi
-watch -n 15 'arp -vn | tee --append $DIR_NAME/arp/$FILE_NAME' &>/dev/null &
-
-if [ ! -d "$DIR_NAME/route" ]; then mkdir "$DIR_NAME/route"; fi
-watch -n 15 'route -n | tee --append $DIR_NAME/route/$FILE_NAME' &>/dev/null &
-
-if [ ! -d "$DIR_NAME/pcaps" ]; then mkdir "$DIR_NAME/pcaps"; fi
-tcpdump -ne -w $DIR_NAME/pcaps/randcrawler.pcap -C 100 &
+if [ ! -d "$ARP_DIR" ]; then mkdir -p "$ARP_DIR"; fi
+if [ ! -d "$RT_DIR/route" ]; then mkdir -p "$RT_DIR"; fi
+if [ ! -d "$CAP_DIR" ]; then mkdir -p "$CAP_DIR"; fi
+crontab /etc/cron.d/monitor-cron
+cron &
+tcpdump -ne -w $CAP_DIR/$(hostname)_$(date +%H-%M-%S).pcap -C 100 &
 
 python3 randcrawler.py
